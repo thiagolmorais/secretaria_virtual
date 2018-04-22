59e01414555b843ca481750e09371e25a6148269
Thiago Fernando <thiago.fernando@locaweb.com.br>
2018-04-22 18:18:37 -0300

    fix model patient

--- spec/features/user_view_patient_spec.rb
+++ spec/features/user_view_patient_spec.rb
@@ -4,12 +4,12 @@ feature 'User see patient name' do
   scenario 'sucessfuly' do
     pending
     user = create(:user)
-    patient = create(:patient)
+    paciente = create(:paciente)
 
     login_as(user)
     visit root_path
     click_on 'Pacientes'
 
-    expect(page).to have_link(patient.name)
+    expect(page).to have_link(paciente.name)
   end
 end
