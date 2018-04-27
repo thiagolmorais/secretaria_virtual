42b3553b004fc63aa4d722791323da0f890a615b
VitorJCarmo <vitor.jesus@creditoo.com.br>
2018-04-27 16:49:15 -0300

    front fixed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,8 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
+# application.js, application.css, mdb.min.css, mdb.min.js and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( material.min.css )
-Rails.application.config.assets.precompile += %w( material.min.js )
+
