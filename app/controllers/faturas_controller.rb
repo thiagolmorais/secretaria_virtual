0230f6f647e00c1f87773b14ce1112858bbfe9f8
Thiago Morais <tf_lima@terra.com.br>
2018-04-30 23:16:14 -0300

    Fix paciente

--- app/controllers/faturas_controller.rb
+++ app/controllers/faturas_controller.rb
@@ -5,4 +5,37 @@ class FaturasController < ApplicationController
     @pacientes = Paciente.all
     @faturas = Fatura.all
   end
+<<<<<<< c8147b88821393b0e3cfd5166137c67ef31117c9
+=======
+
+  def show
+    @faturas = Fatura.find(params[:id])
+  end
+
+  def create
+    @fatura = Fatura.new(fatura_params)
+    @consulta = Consulta.find(params[:consulta_id])
+    @fatura.valor = @consulta.valor_consulta.valor
+    @fatura.vencimento = @fatura.competencia
+    @consulta.status = true
+    @fatura.save
+    @consulta.save
+    redirect_to consultas_path
+  end
+
+  def destroy
+    @fatura = Fatura.find(params[:id])
+    @consulta = Consulta.find(@fatura.consulta_id)
+    @consulta.status = false
+    @consulta.save
+    @fatura.destroy
+    redirect_to consultas_path
+  end
+
+  private
+
+  def fatura_params
+    params.permit(:consulta_id, :competencia)
+  end
+>>>>>>> Fix paciente
 end
