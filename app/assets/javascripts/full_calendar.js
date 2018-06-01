0540a94d8c2345045c66e0fb2b800ac1c1bfc451
Renan Pinto <renanspinto@hotmail.com>
2018-05-31 21:16:20 -0300

    retirada de um grafico

--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -48,4 +48,48 @@ $('#calendar').each(function  (){
        }
     });
 });
+
+// CALENDARIO HOME
+  $('#calendar-home').each(function() {
+    var calendar = $(this);
+    calendar.fullCalendar({
+      defaultView: "listDay",
+      header: {
+
+        // center: 'title',
+        // right: 'month,agendaWeek,agendaDay,listWeek'
+      },
+      selectable: true,
+      selectHelper: true,
+      editable: true,
+      eventLimit: true,
+      events: 'consultas.json',
+
+      select: function(start, end) {
+
+        window.location.href = "consultas/new";
+
+        calendar.fullCalendar('unselect');
+      },
+
+      eventDrop: function(event, delta, revertFunc) {
+        event_data = {
+          event: {
+            id: event.id,
+            start: event.start.format(),
+            end: event.end.format()
+          }
+        };
+        $.ajax({
+          url: event.update_url,
+          data: event_data,
+          type: 'PATCH'
+        });
+      },
+
+      eventClick: function(event, jsEvent, view) {
+        window.location.href = event.edit_url;
+      }
+    });
+  });
 })
