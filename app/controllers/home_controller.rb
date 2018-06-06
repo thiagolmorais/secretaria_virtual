a459f60dc5677927f3b709e887da07d18bffb038
Vinicius Loreto <vini.loreto@gmail.com>
2018-06-06 01:02:51 -0300

    Arrumado side menu de configuração/ Paginação home / Paginação 'personalizada' / Erros de botões e escrita

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    @pacientes = Paciente.all.order(:nome)
+    @pacientes = Paciente.all.order(:nome).paginate(page: params[:page], per_page: 5)
   end
 end
